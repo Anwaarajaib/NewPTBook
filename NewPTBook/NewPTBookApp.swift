01f8ad8cb44d01bbfa4346582879138ad92529fe
Anwaar <anwaarajaib@gmail.com>
2024-11-30 16:43:31 +0400

    Initial Commit

---
+++ NewPTBook/NewPTBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewPTBookApp.swift
+//  NewPTBook
+//
+//  Created by Mohammed Anwaar Ajaib on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewPTBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewPTBookTests/NewPTBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewPTBookTests.swift
+//  NewPTBookTests
+//
+//  Created by Mohammed Anwaar Ajaib on 30/11/2024.
+//
+
+import Testing
+@testable import NewPTBook
+
+struct NewPTBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
